22599c53a0a29e4a5b3f8044213417a8cf3218cc
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-31 10:01:44 -0500

    Refactor welcome message for clarity

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Team Collaboration Dashboard!');
+  res.send('Welcome to the Team Collaboration Dashboard! Enjoy collaborating.');
 });
 
 io.on('connection', (socket) => {
